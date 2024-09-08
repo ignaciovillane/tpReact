170da9c49f569d730c1c2a59cc10ced6b31821b3
ignacio villane <ignaciovillane@gmail.com>
2024-09-08 01:01:56 -0300

    primer commit

---
+++ tp6/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PaymentForm from './PaymentForm';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PaymentForm />
+    </div>
+  );
+}
+
+export default App;
